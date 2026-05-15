1630c1080468b46a29291b2722b9edd2f3df6ea2
Mahesh Rao <maheshrao@Maheshs-MacBook-Air.local>
2026-05-15 22:27:39 +0530

    updated workflows

--- alb/outputs.tf
+++ alb/outputs.tf
@@ -6,10 +6,3 @@ output "target_groups" {
   value = module.alb.target_groups
 }
 
-output "green_dns_name" {
-  value = module.alb.listeners["test"].dns_name
-}
-
-output "listeners" {
-  value = module.alb.listeners
-}
